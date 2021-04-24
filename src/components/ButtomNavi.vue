199372c8310b6cf73712b34c0c1e23f376071476
PommesPeter <434596665@qq.com>
2021-04-24 23:14:58 +0800

    add widgert

---
+++ src/components/ButtomNavi.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-bottom-navigation
+      :value="value"
+      color="primary"
+  >
+    <v-btn>
+      <span>Recent</span>
+
+      <v-icon>mdi-history</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Favorites</span>
+
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Nearby</span>
+
+      <v-icon>mdi-map-marker</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+  name: "NaviDrawer",
+  data: () => ({value: 1}),
+}
+</script>
+
+<style scoped>
+
+</style>
