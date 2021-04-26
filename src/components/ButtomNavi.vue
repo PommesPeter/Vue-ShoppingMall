69a3ab0ed565df59fa1090e2c2effa9ae79220f7
PommesPeter <434596665@qq.com>
2021-04-26 16:49:02 +0800

    update Buttom navi

--- src/components/ButtomNavi.vue
+++ src/components/ButtomNavi.vue
@@ -2,28 +2,25 @@
   <v-bottom-navigation
       :value="value"
       color="primary"
+      hide-on-scroll
   >
-    <v-btn>
+    <v-btn style="height: auto">
       <span>注册/登录</span>
-
       <v-icon>mdi-login</v-icon>
     </v-btn>
 
-    <v-btn>
+    <v-btn style="height: auto">
       <span>商品列表</span>
-
       <v-icon>mdi-shopping</v-icon>
     </v-btn>
 
-    <v-btn>
+    <v-btn style="height: auto">
       <span>购物车</span>
-
       <v-icon>mdi-cart</v-icon>
     </v-btn>
 
-    <v-btn>
+    <v-btn style="height: auto">
       <span>订单管理</span>
-
       <v-icon>mdi-reorder-horizontal</v-icon>
     </v-btn>
   </v-bottom-navigation>
@@ -37,5 +34,6 @@ export default {
 </script>
 
 <style scoped>
+
 
 </style>
