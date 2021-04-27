1557532527d806a4b946a96a0f9a6bc7b809d342
PommesPeter <434596665@qq.com>
2021-04-27 22:32:09 +0800

    add login style

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,44 +1,96 @@
 <template>
-  <form>
-    <v-text-field
-        v-model="name"
-        :error-messages="nameErrors"
-        :counter="10"
-        label="Name"
-        required
-        @input="$v.name.$touch()"
-        @blur="$v.name.$touch()"
-    ></v-text-field>
-    <v-text-field
-        v-model="email"
-        :error-messages="emailErrors"
-        label="E-mail"
-        required
-        @input="$v.email.$touch()"
-        @blur="$v.email.$touch()"
-    ></v-text-field>
+  <v-container class="container">
+    <form>
+      <v-text-field
+          v-model="userName"
+          :error-messages="nameErrors"
+          :counter="20"
+          label="userName"
+          required
+          @input="$v.name.$touch()"
+          @blur="$v.name.$touch()"
+          style="justify-content: center; width: auto"
+      ></v-text-field>
+      <v-text-field
+          v-model="passWord"
+          :error-messages="emailErrors"
+          label="E-mail"
+          required
+          @input="$v.email.$touch()"
+          @blur="$v.email.$touch()"
+          style="justify-content: center; width: auto;"
+      ></v-text-field>
 
-    <v-btn
-        class="mr-4"
-        @click="submit"
-    >
-      submit
-    </v-btn>
-    <v-btn @click="clear">
-      clear
-    </v-btn>
-  </form>
+      <div class="footer">
+        <a class="forget password" href="#">Forgot Password?</a>
+      </div>
+
+      <v-btn
+          class="mr-4"
+          @click="submit"
+      >
+        submit
+      </v-btn>
+      <v-btn @click="clear">
+        clear
+      </v-btn>
+    </form>
+  </v-container>
+
 </template>
 
 <script>
+import {validationMixin} from 'vuelidate'
+import {required, maxLength, email} from 'vuelidate/lib/validators'
+
 export default {
   name: "LoginPage",
-  data() {
-    return {
-      isShow: true
-    }
+  mixins: [validationMixin],
+
+  validations: {
+    name: {required, maxLength: maxLength(10)},
+    email: {required, email},
+    select: {required},
+    checkbox: {
+      checked(val) {
+        return val
+      },
+    },
   },
+
+  data: () => ({
+    userName: '',
+    passWord: '',
+  }),
+
+  computed: {
+    nameErrors() {
+      const errors = []
+      if (!this.$v.name.$dirty) return errors
+      !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
+      !this.$v.name.required && errors.push('Name is required.')
+      return errors
+    },
+    emailErrors() {
+      const errors = []
+      if (!this.$v.email.$dirty) return errors
+      !this.$v.email.email && errors.push('Must be valid e-mail')
+      !this.$v.email.required && errors.push('E-mail is required')
+      return errors
+    },
+  },
+
   methods: {
+    submit() {
+      this.$v.$touch()
+    },
+    clear() {
+      this.$v.$reset()
+      this.name = ''
+      this.email = ''
+      this.select = null
+      this.checkbox = false
+    },
     regiSter() {
       this.$axios.post('http://202.193.52.12:8080/user/register?name=jianhgao3&password=123456')
           .then(res => {
@@ -52,83 +104,28 @@ export default {
             console.log(error)
           })
     }
-  }
-}
-</script><script>
-import { validationMixin } from 'vuelidate'
-import { required, maxLength, email } from 'vuelidate/lib/validators'
-
-export default {
-  mixins: [validationMixin],
-
-  validations: {
-    name: { required, maxLength: maxLength(10) },
-    email: { required, email },
-    select: { required },
-    checkbox: {
-      checked (val) {
-        return val
-      },
-    },
-  },
-
-  data: () => ({
-    name: '',
-    email: '',
-    select: null,
-    items: [
-      'Item 1',
-      'Item 2',
-      'Item 3',
-      'Item 4',
-    ],
-    checkbox: false,
-  }),
-
-  computed: {
-    checkboxErrors () {
-      const errors = []
-      if (!this.$v.checkbox.$dirty) return errors
-      !this.$v.checkbox.checked && errors.push('You must agree to continue!')
-      return errors
-    },
-    selectErrors () {
-      const errors = []
-      if (!this.$v.select.$dirty) return errors
-      !this.$v.select.required && errors.push('Item is required')
-      return errors
-    },
-    nameErrors () {
-      const errors = []
-      if (!this.$v.name.$dirty) return errors
-      !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
-      !this.$v.name.required && errors.push('Name is required.')
-      return errors
-    },
-    emailErrors () {
-      const errors = []
-      if (!this.$v.email.$dirty) return errors
-      !this.$v.email.email && errors.push('Must be valid e-mail')
-      !this.$v.email.required && errors.push('E-mail is required')
-      return errors
-    },
-  },
-
-  methods: {
-    submit () {
-      this.$v.$touch()
-    },
-    clear () {
-      this.$v.$reset()
-      this.name = ''
-      this.email = ''
-      this.select = null
-      this.checkbox = false
-    },
   },
 }
 </script>
 
 <style scoped>
+.container {
+  background: #f6f6f7;
+  border-radius: 10px 10px 10px 10px;
+  width: 80%;
+  font-size: 20px;
+  position: relative;
+  -webkit-box-shadow: 0 30px 60px 0 rgba(0, 0, 0, 0.3);
+  box-shadow: 0 30px 60px 0 rgba(0, 0, 0, 0.3);
+  max-width: 450px;
+}
 
+.footer {
+  background-color: #f6f6f6;
+  border-top: 1px solid #dce8f1;
+  padding: 25px;
+  text-align: center;
+  -webkit-border-radius: 0 0 10px 10px;
+  border-radius: 0 0 10px 10px;
+}
 </style>
